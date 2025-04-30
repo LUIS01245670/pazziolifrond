3c9a48ce746afe6c786d179ecafbe151bf2c7fd3
cavsystems <desarrollo@cavsystems.com.co>
2025-04-30 12:12:32 -0500

    cambioip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  vendedor:"token_vendedor",
-  api:"http://localhost:3000"
-
-
+  vendedor: 'token_vendedor',
+  api: 'http://192.168.238.5:3000',
 };
