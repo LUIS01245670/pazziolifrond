e1d72e4c788417b4af95e19eeef9e2af1f48f312
cavsystems <desarrollo@cavsystems.com.co>
2025-05-06 13:08:15 -0500

    actulizacioncode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   vendedor: 'token_vendedor',
-  api: 'https://pazzioli.cavsystems.com.co/api',
+  api: 'https://pazzioliweb.cavsystems.com.co/api',
   apisocket: 'https://pazzioliweb.cavsystems.com.co',
 };
